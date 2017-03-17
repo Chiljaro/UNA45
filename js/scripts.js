a91a96d07e178dcebd7aa1413c937488a0b28637
root <gonzalez_9411@hotmail.com>
2017-03-17 08:50:41 -0600

    pishe vieja ya quedo su pishe slider mamon, pasame ua chela chinga

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 edgrid.menu('main-nav','main-menu');
 
-//slide
 
+//menu
 jQuery(document).ready(function(){
     $('h2').append('<a href="#top" class="gototop">Subir</a>');
     linkInterno = $('a[href^="#"]');
@@ -15,18 +15,37 @@ jQuery(document).ready(function(){
 var slideIndex = 0;
 showSlides();
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 6000); // Change image every 2 seconds
+
+//slide
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
 
 
 /*!
